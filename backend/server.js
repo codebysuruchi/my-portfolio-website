a6ad56fad64cc4d2b437eb02a18db114f6650042
codebysuruchi <suruchijha320@mail.com>
2025-09-08 18:59:02 +0530

    fixed connection issues

--- backend/server.js
+++ backend/server.js
@@ -10,12 +10,17 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({
   origin: [
   "https://my-portfolio-website-alpha-five.vercel.app",
-  "http://localhost:5000"  // for local development
+  "http://localhost:5000" , // for local development
+  "http://localhost:5173"   // If using Vite
 ],
   credentials: true
 }));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
+
+if (process.env.NODE_ENV === 'production') {
+  app.set('trust proxy', 1);
+   }
 
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI || "mongodb+srv://suruchijha:ayush21012004@cluster0.0qhefk6.mongodb.net/portfolio", {
@@ -121,10 +126,6 @@ app.post("/api/contact", validateContactInput, async (req, res) => {
         message: "This email has already sent a message recently" 
       });
     }
-
-    if (process.env.NODE_ENV === 'production') {
-  app.set('trust proxy', 1);
-   }
     
     res.status(500).json({ 
       success: false, 
