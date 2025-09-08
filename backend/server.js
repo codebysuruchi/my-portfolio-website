95afc47158eff02ad63a8bde99d12052c94318f0
codebysuruchi <suruchijha320@mail.com>
2025-09-08 19:46:26 +0530

    change in server

--- backend/server.js
+++ backend/server.js
@@ -25,6 +25,9 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],  
   allowedHeaders: ['Content-Type', 'Authorization']       
 }));
+
+app.options('*', cors());
+
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 
